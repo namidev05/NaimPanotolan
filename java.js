2f6f6956f5153253153f1302027a0647f0ba5854
Naim Panotolan <naimpanotolan@gmail.com>
2020-08-14 20:39:25 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,60 @@
+const abouts = document.querySelectorAll('.fade-in');
+const pro = document.querySelectorAll('.fade-in1');
+const contact = document.querySelectorAll('.fade-in2');
+
+const appearoptions = {
+    thread: 0,
+    rootMargin: "0px 0px -250px 0px"  
+};
+
+const appearsOnscroll = new IntersectionObserver(
+    function(
+        entries,
+        appearsOnscroll)
+        {entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            }else{
+                entry.target.classList.add('appear');
+                appearsOnscroll.unobserve(entry.target);
+            }
+        })
+
+        },
+appearoptions);
+
+
+abouts.forEach(abouts => {
+    appearsOnscroll.observe(abouts);
+})
+
+
+
+const appearoptions1 = {
+    thread: 0,
+    rootMargin: "0px 0px -200px 0px"  
+};
+
+const appearsOnscroll1 = new IntersectionObserver(
+    function(
+        entries,
+        appearsOnscroll1)
+        {entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            }else{
+                entry.target.classList.add('appear');
+                appearsOnscroll1.unobserve(entry.target);
+            }
+        })
+
+        },
+appearoptions1);
+
+pro.forEach(pro => {
+    appearsOnscroll1.observe(pro);
+});
+
+contact.forEach(contact => {
+    appearsOnscroll1.observe(contact);
+});
